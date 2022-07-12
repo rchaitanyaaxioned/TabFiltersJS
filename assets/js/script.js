f5c2ea03c6681156d80fd7881c0b896e0117d865
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-12 16:47:24 +0530

    JS logic working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
-var tab = document.querySelector("tab");
-console.log(tab);
+const tabs = document.querySelectorAll(".tab");
+const liContent = document.querySelectorAll(".li-content");
+const tabAnchor = document.querySelectorAll("a");
 
-tab.addEventListener("onclick", dispalyContent());
+tabs.forEach(dispalyContent);
 
-function dispalyContent() {
+function dispalyContent(item, index) {
+  item.onclick = function () {
+    let result = item.classList.contains("active-li");
 
+    if (result == true) {
+      item.classList.remove("active-li");
+      liContent[index].classList.remove("active-tab");
+      tabAnchor[index].classList.remove("active-a");
+    } else {
+      item.classList.add("active-li");
+      liContent[index].classList.add("active-tab");
+      tabAnchor[index].classList.add("active-a");
+    }
+  }
 }
+
+
 
 
 
