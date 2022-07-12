6d81c1b67074c38d095bdae59dcb323f99a5c01c
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-12 17:21:09 +0530

    Before making changes in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 const tabs = document.querySelectorAll(".tab");
 const liContent = document.querySelectorAll(".li-content");
-const tabAnchor = document.querySelectorAll("a");
+const tabAnchor = document.getElementsByTagName("a");
 
 tabs.forEach(dispalyContent);
 
 function dispalyContent(item, index) {
+
   item.onclick = function () {
+
+    // item.classList.add("active-li");
+    // liContent[index].classList.add("active-tab");
+    // tabAnchor[index].classList.add("active-a");
+
     let result = item.classList.contains("active-li");
 
     if (result == true) {
