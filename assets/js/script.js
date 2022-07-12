7a4e484af9a86f81dfd802ccaf9efc76a18c1ac6
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-12 14:50:38 +0530

    Before fixing tab-content display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var tabLeadership = document.querySelector("tab-leadership");
-var tabHistory = document.querySelector("tab-history");
-var tabSustainability = document.querySelector("tab-sustainability");
+var tab = document.querySelector("tab");
+
+
 
 
 
