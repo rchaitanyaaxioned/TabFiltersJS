9fce05a9c7626df949c972048c1c132f29cff15e
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-13 18:22:36 +0530

    JS logic corrected

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+const tabs = document.querySelectorAll(".tab");
+const aTab = document.querySelectorAll(".tab-a");
+const tabContent = document.querySelectorAll(".tab-content");
+const btn = document.querySelectorAll(".btn");
+const imgs = document.querySelectorAll(".all-img");
+
+//for activating tab
+tabs.forEach(selectTab);
+
+function selectTab(item, index) {
+
+  item.addEventListener("click", activateTab);
+
+  function activateTab() {
+
+    //for item in tab content
+    tabs.forEach(removeClass);
+
+    function removeClass(ritem, ridx) {
+      ritem.classList.remove("active-tab");
+      aTab[ridx].classList.remove("active-a");
+      tabContent[ridx].classList.remove("show");
+    }
+
+    item.classList.add("active-tab");
+    aTab[index].classList.add("active-a");
+    tabContent[index].classList.add("show");
+  }
+}
+
+//for activating btn
+btn.forEach(selectBtn);
+
+function selectBtn(item, index) {
+
+  item.addEventListener("click", activateBtn);
+
+  function activateBtn() {
+
+    //for item in tab content
+    btn.forEach(removeClass);
+
+    function removeClass(ritem, ridx) {
+      ritem.classList.remove("active-btn");
+    }
+
+    item.classList.add("active-btn");
+    let tabValue = item.getAttribute("data-value-tab");
+    for (let i = 0; i <= imgs.length; i++) {
+      let imgValue = imgs[i].getAttribute("data-value-img");
+      if (tabValue == imgValue) {
+        imgs[i].classList.remove("hide");
+        imgs[i].classList.add("show");
+      } else {
+        imgs[i].classList.remove("show");
+        imgs[i].classList.add("hide");
+      }
+      console.log(imgValue);
+    }
+  }
+}
