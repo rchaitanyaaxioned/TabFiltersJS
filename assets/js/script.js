9be8ca6c3411871a1a5346c8444590be119acbbd
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-13 11:51:57 +0530

    All js logic is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@ const tabs = document.querySelectorAll(".tab");
 const liContent = document.querySelectorAll(".li-content");
 const tabAnchor = document.getElementsByTagName("a");
 const btn = document.querySelectorAll(".btn");
+const allImg = document.querySelectorAll(".all-img");
+const buisness = document.querySelectorAll(".buisness");
+const eco = document.querySelectorAll(".eco");
+const controller = document.querySelectorAll(".controller");
+const textEffect = document.querySelectorAll(".text-effect");
 
 //for tab from tab-list
 tabs.forEach(dispalyContent);
@@ -23,6 +28,58 @@ function dispalyContent(item, index) {
     item.classList.add("active-tab");
     liContent[index].classList.add("active-li");
     tabAnchor[index].classList.add("active-a");
+  }
+}
+
+btn.forEach(activeBtn);
+
+function activeBtn(btnItem, btnIndex) {
+
+  btnItem.addEventListener("click", btnActivate);
+
+  function btnActivate() {
+    //for item in tab content
+    btn.forEach(removeActive);
+
+    function removeActive(activeItem, activeIdx) {
+      activeItem.classList.remove("active-btn");
+    }
+    btnItem.classList.add("active-btn");
+    console.log("btnIndex : " + btnIndex);
+
+    allImg.forEach(imgFun);
+    function imgFun(imgItem) { imgItem.style.display = "none"; }
+
+    //for filtering imges
+    switch (btnIndex) {
+      case 0:
+        allImg.forEach(imgFun);
+        function imgFun(imgItem) { imgItem.style.display = "block"; }
+        break;
+
+      case 1:
+        buisness.forEach(imgFun);
+        function imgFun(imgItem) { imgItem.style.display = "flex"; }
+        break;
+
+      case 2:
+        eco.forEach(imgFun);
+        function imgFun(imgItem) { imgItem.style.display = "flex"; }
+        break;
+
+      case 3:
+        controller.forEach(imgFun);
+        function imgFun(imgItem) { imgItem.style.display = "flex"; }
+        break;
+
+      case 4:
+        textEffect.forEach(imgFun);
+        function imgFun(imgItem) { imgItem.style.display = "flex"; }
+        break;
+
+      default:
+        allImg.style.display = "none";
+    }
   }
 }
 
