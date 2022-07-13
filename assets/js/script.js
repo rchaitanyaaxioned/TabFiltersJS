dd6c5149646abfc4c19389575f389b54d4f845f4
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-13 06:33:22 +0530

    Before making changes is css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const tabs = document.querySelectorAll(".tab");
 const liContent = document.querySelectorAll(".li-content");
 const tabAnchor = document.getElementsByTagName("a");
+const btn = document.querySelector(".btn");
 
 tabs.forEach(dispalyContent);
 
@@ -8,21 +9,21 @@ function dispalyContent(item, index) {
 
   item.onclick = function () {
 
-    // item.classList.add("active-li");
-    // liContent[index].classList.add("active-tab");
-    // tabAnchor[index].classList.add("active-a");
+    item.classList.toggle("active-tab");
+    liContent[index].classList.toggle("active-li");
+    tabAnchor[index].classList.toggle("active-a");
 
-    let result = item.classList.contains("active-li");
+    // let result = item.classList.contains("active-li");
 
-    if (result == true) {
-      item.classList.remove("active-li");
-      liContent[index].classList.remove("active-tab");
-      tabAnchor[index].classList.remove("active-a");
-    } else {
-      item.classList.add("active-li");
-      liContent[index].classList.add("active-tab");
-      tabAnchor[index].classList.add("active-a");
-    }
+    // if (result == true) {
+    //   item.classList.remove("active-tab");
+    //   liContent[index].classList.remove("active-li");
+    //   tabAnchor[index].classList.remove("active-a");
+    // } else {
+    //   item.classList.add("active-tab");
+    //   liContent[index].classList.add("active-li");
+    //   tabAnchor[index].classList.add("active-a");
+    // }
   }
 }
 
