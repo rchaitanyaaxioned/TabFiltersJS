9edccd5436e7931d0d2135927045b8989a6fc0b5
rchaitanyaaxioned <chaitanyar@axioned.com>
2022-07-13 06:55:43 +0530

    Tab active logic fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 const tabs = document.querySelectorAll(".tab");
 const liContent = document.querySelectorAll(".li-content");
 const tabAnchor = document.getElementsByTagName("a");
-const btn = document.querySelector(".btn");
+const btn = document.querySelectorAll(".btn");
 
 tabs.forEach(dispalyContent);
 
@@ -9,21 +9,32 @@ function dispalyContent(item, index) {
 
   item.onclick = function () {
 
-    item.classList.toggle("active-tab");
-    liContent[index].classList.toggle("active-li");
-    tabAnchor[index].classList.toggle("active-a");
+    // item.classList.toggle("active-tab");
+    // liContent[index].classList.toggle("active-li");
+    // tabAnchor[index].classList.toggle("active-a");
 
-    // let result = item.classList.contains("active-li");
-
-    // if (result == true) {
-    //   item.classList.remove("active-tab");
-    //   liContent[index].classList.remove("active-li");
-    //   tabAnchor[index].classList.remove("active-a");
-    // } else {
-    //   item.classList.add("active-tab");
-    //   liContent[index].classList.add("active-li");
-    //   tabAnchor[index].classList.add("active-a");
+    // btn.forEach(check);
+    // function check(i, idx) {
+    //   i.onclick = function () {
+    //     if (i.classList.contains("active-btn") == true) {
+    //       i.classList.remove("active-btn");
+    //     } else {
+    //       i.classList.add("active-btn");
+    //     }
+    //   }
     // }
+
+    let result = item.classList.contains("active-tab");
+
+    if (result == true) {
+      item.classList.remove("active-tab");
+      liContent[index].classList.remove("active-li");
+      tabAnchor[index].classList.remove("active-a");
+    } else {
+      item.classList.add("active-tab");
+      liContent[index].classList.add("active-li");
+      tabAnchor[index].classList.add("active-a");
+    }
   }
 }
 
